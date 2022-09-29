f6b392ef450b7eaa2150e253b6f902cb74b099db
Nicholas English <Nick.English@autosweet.com>
2022-09-28 20:51:19 -0400

    Missed code in commit [GH-3]

--- TankGame/Assets/Scripts/PlayerBody.cs
+++ TankGame/Assets/Scripts/PlayerBody.cs
@@ -51,28 +51,7 @@ public class PlayerBody : MonoBehaviour
         }
     }
 
-    private void Move()
-    {
-        float horizontalSpeed = Input.GetAxisRaw("Horizontal") * acceleration;
-        float verticalSpeed = Input.GetAxisRaw("Vertical") * acceleration;
 
-        // Accelerate the player
-        if (horizontalSpeed != 0 || verticalSpeed != 0)
-        {
-            movement = new Vector2(horizontalSpeed, verticalSpeed);
-            rigidbody_2D.AddForce(movement);
-        }
-
-        // Cap player speed
-        if (rigidbody_2D.velocity.magnitude > maxSpeed)
-        {
-            rigidbody_2D.velocity = rigidbody_2D.velocity.normalized * maxSpeed;
-        }
-
-        // Decelerate the player
-        movement = new Vector2(rigidbody_2D.velocity.x * deceleration, rigidbody_2D.velocity.y * deceleration);
-        rigidbody_2D.AddForce(movement);
-    }
 
     // Update is called once per frame
     public void Update() 
