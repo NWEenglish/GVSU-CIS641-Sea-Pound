a4e0af013e5e46567ed59ed102d19ed19956c498
Nicholas <englishnicholas@yahoo.com>
2022-10-10 21:48:23 -0400

    Add audio for enemy movement [GH-9]

--- TankGame/Assets/Scripts/EnemyMovementLogic.cs
+++ TankGame/Assets/Scripts/EnemyMovementLogic.cs
@@ -12,11 +12,26 @@ public class EnemyMovementLogic : MonoBehaviour
     public EnemyType EnemyType;
 
     private Rigidbody2D Body;
+    private AudioSource audioSource_Idle;
+    private AudioSource audioSource_Moving;
 
     // Start is called before the first frame update
     void Start()
     {
         Body = gameObject.GetComponent<Rigidbody2D>();
+
+        // Setup Audio Objects
+        var audioSources = gameObject.GetComponents<AudioSource>();
+        audioSource_Idle = audioSources[0];
+        audioSource_Moving = audioSources[1];
+
+        audioSource_Moving.loop = true;
+        audioSource_Moving.Play();
+        audioSource_Moving.volume = 0.2f;
+
+        audioSource_Idle.loop = true;
+        audioSource_Idle.Play();
+        audioSource_Idle.volume = 0.1f;
     }
 
     void FixedUpdate()
@@ -31,19 +46,26 @@ public class EnemyMovementLogic : MonoBehaviour
             // Move towards player
             if (target.magnitude <= StartChaseRange && target.magnitude >= StopChaseRange)
             {
+                // Movement Logic
                 MovementHelper.Move(ref Body, target.x, target.y, MaxSpeed);
                 MovementHelper.Rotate(ref Body, target, -90f);
+
+                // Audio Logic
+                audioSource_Idle.mute = true;
+                audioSource_Moving.mute = false;
             }
             else
             {
+                // Movement Logic
                 MovementHelper.Move(ref Body, 0, 0, 3f);
+
+                // Audio Logic
+                audioSource_Idle.mute = false;
+                audioSource_Moving.mute = true;
             }
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
