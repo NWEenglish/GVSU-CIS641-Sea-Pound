1929b610cd79ed693e01742a340155836a6e1851
Nicholas <englishnicholas@yahoo.com>
2022-11-21 00:04:47 -0500

    In Defense mode: added kill count, gradually decrease spawn time [gh-30]

--- TankGame/Assets/Scripts/GeneralGameLogic/GameModeSetup.cs
+++ TankGame/Assets/Scripts/GeneralGameLogic/GameModeSetup.cs
@@ -12,13 +12,19 @@ namespace Assets.Scripts.GeneralGameLogic
     {
         public GameModeType GameMode { get; private set; }
         public GameModeObjectives GameModeObjectives { get; private set; }
+        public DefenseKillCount DefenseKillCount { get; private set; }
 
         void Start()
         {
             GameMode = GetGameModeBySceneName();
             GameModeObjectives = new GameModeObjectives(GameMode);
 
-            int seed = 421114010; //(int)(new System.Random().NextDouble() * 1000000000);
+            if (GameMode == GameModeType.Defensive)
+            {
+                DefenseKillCount = new DefenseKillCount();
+            }
+
+            int seed = (int)(new System.Random().NextDouble() * 1000000000);
             UnityEngine.Random.InitState(seed);
 
             GameObject.Find(HUDNames.Seed).GetComponent<TextMeshProUGUI>().text = $"Seed: {seed}";
--- TankGame/Assets/Scripts/Objective/ObjectiveHUDLogic.cs
+++ TankGame/Assets/Scripts/Objective/ObjectiveHUDLogic.cs
@@ -12,11 +12,13 @@ namespace Assets.Scripts.Objective
         public GameObject Objective_HUD;
 
         private GameModeObjectives GameModeObjectives;
+        private DefenseKillCount DefenseKillCount;
 
         void Start()
         {            
             Objective_HUD.GetComponent<TextMeshProUGUI>().text = "";
             GameModeObjectives = GameObject.Find(ObjectNames.GameLogic).GetComponent<GameModeSetup>().GameModeObjectives;
+            DefenseKillCount = GameObject.Find(ObjectNames.GameLogic).GetComponent<GameModeSetup>().DefenseKillCount;
         }
 
         void Update()
@@ -33,6 +35,11 @@ namespace Assets.Scripts.Objective
                 if (objectives[index].Completed)
                 {
                     text = $"<color=green><alpha=#CC>{text}</color>";
+                }
+
+                if (DefenseKillCount != null)
+                {
+                    text += $"\n\n\u2022<indent=1em>Kill Count: {DefenseKillCount.KillCount}</indent>";
                 }
 
                 Objective_HUD.GetComponent<TextMeshProUGUI>().text += $"{text}\n\n";
